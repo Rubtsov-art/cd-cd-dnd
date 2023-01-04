ecd0915ec319a8b18f53639c06daab8bdb7e8f51
pavel <pasharub60@gmail.com>
2023-01-04 19:59:33 +0400

    remove import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import dndForWeb from './dndForWeb';
 import dndForMobile from './dndForMobile';
-import "../style.css";
 
 const ball = document.getElementById('ball-id');
 const field = document.getElementById('field-id');
